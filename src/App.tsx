113757c632b7894db4f2fd59459d50597215baef
Leksomir <aleksa47@gmail.com>
2021-07-29 11:29:36 +0200

    Fix github pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Favorites from './pages/favorites/Favorites';
 import Search from './pages/search/Search';
 
 function App() {
   return (
-    <Router>
+    <HashRouter basename="/">
       <Navbar />
 
       <div className="section">
@@ -20,7 +20,7 @@ function App() {
           </Switch>
         </div>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
